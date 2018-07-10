8f9e35454ec1d38b2cfcced076f4b20f7565e1a4
sharmindran <sharmindranraj@gmail.com>
2018-07-10 12:59:35 +0800

    updated front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import Main from './components/Main';
+import Historical from './components/Historical';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Navbar />
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/form" component={Form} />
+            <Route exact path="/historical" component={Historical} />
+          </Switch>
+        </div>
       </div>
     );
   }
---
+++ client/src/components/Historical.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Historical extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Historical Work</h1>
+      </div>
+    );
+  }
+}
+
+export default Historical;
